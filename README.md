0c6708dda217900d129128c14dcea2d409bb7e1b
Erika Debelis <Erika.Debelis@gmail.com>
2021-06-14 15:42:53 -0700

    create to-do list object + html form

---
+++ README.md
@@ -0,0 +1,20 @@
+// to-do list
+//specific behaviors- create task, remove task, 
+
+
+
+
+Describe: toDoList()
+Test: "It should create a constructor called ToDoList"
+Code: let newList = new ToDoList();
+Expected Output: newList {chore: {…}}
+
+Describe: chore()
+Test: "It should create a constructor called Chore"
+Code: let task1 = new Chore();
+Expected Output: Choret {name: " {}...}}
+
+Describe: addChore()
+Test: "It should add a chore to a ToDoList"
+Code: newList.addChore('garbage')
+Expected Output: ToDoList {chore: {…}} chore: {groceries: Chore}
---
+++ js/scripts.js
@@ -0,0 +1,87 @@
+// to-do list
+//specific behaviors- create task, remove task, 
+
+function ToDoList() {
+  this.chores = {};
+  this.currentId = 0
+}
+
+ToDoList.prototype.addChore = function(chore) {
+  chore.id = this.assignId();
+  this.chores[chore.id] = chore;
+}
+
+function Chore(choreName) {
+  this.name = choreName;
+  this.done = false;
+}
+
+ToDoList.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+}
+
+// ToDoList.prototype.deleteChore = function(id) {
+//   if (this.chores[id] === false) {
+//     return false;
+//   }
+//   return true;
+// }
+
+
+/*
+let laundry = new Chore("laundry");
+let groceries = new Chore("groceries");
+let newList = new ToDoList();
+newList.addChore(laundry)
+newList.addChore(groceries)
+*/
+
+let list = new ToDoList();
+
+$(document).ready(function() { 
+  $("form#formOne").submit(function(event) {
+    event.preventDefault();
+
+
+    let newTask = $("#noteInput").val();
+    let chore = new Chore(newTask)
+    list.addChore(chore)
+    console.log(list)
+    
+    $("#choreList").prepend("<li>" + newTask + "</li>");
+
+
+    
+  }); 
+});
+
+
+//addressBook.findContact("2");
+//ToDoList.findChore("");
+//// Business Logic for AddressBook ---------
+// function AddressBook() {
+//   this.contacts = {};
+//   this.currentId = 0;
+// }
+
+// AddressBook.prototype.addContact = function(contact) {
+//   contact.id = this.assignId();
+//   this.contacts[contact.id] = contact;
+// };
+
+// AddressBook.prototype.assignId = function() {
+//   this.currentId += 1;
+//   return this.currentId;
+// };
+
+// // Business Logic for Contacts ---------
+// function Contact(firstName, lastName, phoneNumber) {
+//   this.firstName = firstName;
+//   this.lastName = lastName;
+//   this.phoneNumber = phoneNumber;
+// }
+
+// Contact.prototype.fullName = function() {
+//   return this.firstName + " " + this.lastName;
+// };
